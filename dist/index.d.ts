5b0c628e3b34dcc006d5061a480b2c6587655fef
Billy Bryant <3013565+billyjbryant@users.noreply.github.com>
2025-05-15 12:17:19 -0700

    Adding compile to husky pre-commit

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * GitHub Repository Analyzer
+ * A tool for analyzing GitHub repositories for sustainability and security risks.
+ *
+ * This module exports the core functionality of the analyzer.
+ */
+import { analyzeGitHubRepository } from './src/analyzer';
+export { analyzeGitHubRepository };
